33d7ec080b10949e80e92d7bd63445a11d25b639
Aisosa <allysonaghedo@gmail.com>
2022-11-13 21:00:06 +0100

    Airbnb clone

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def states_list():
     """display the list of all states"""
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
