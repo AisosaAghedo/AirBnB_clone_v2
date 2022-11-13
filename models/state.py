47d70c9dd68d4d53af32b6b0c24bd558e96c6b39
Aisosa <allysonaghedo@gmail.com>
2022-11-13 13:46:15 +0100

    Airbnc clone

--- models/state.py
+++ models/state.py
@@ -5,15 +5,16 @@ from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship("City", backref="state", cascade="delete")
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
